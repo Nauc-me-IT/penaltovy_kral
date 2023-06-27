571a20c0a577780ea16c196c2de616566652bcfb
Monika Kinclová <kinclova.monca@gmail.com>
2023-06-27 13:41:59 +0200

    Aktualizace data.js vrací už Pole polí

--- pages/api/data.js
+++ pages/api/data.js
@@ -6,23 +6,21 @@ export default async function handler(req, res) {
   );
   doc.useApiKey("AIzaSyBP6A-1rMWSR2Oi2maHP0KIk73Nn5_Psbc");
   await doc.getInfo();
-  console.log(doc.title);
 
-  const sheet = doc.sheetsByIndex[0]; // or use `doc.sheetsById[id]` or `doc.sheetsByTitle[title]`
-  console.log(sheet.title);
-  console.log(sheet.rowCount);
+  const sheet = doc.sheetsByIndex[0];
+  await sheet.loadCells("A2:J50");
 
-  await sheet.loadCells("A1:J50");
-  console.log(sheet.cellStats); // total cells, loaded, how many non-empty
-  const a1 = sheet.getCell(0, 0); // access cells using a zero-based index
-  const c6 = sheet.getCellByA1("C6"); // or A1 style notation
-  // access everything about the cell
-  console.log(a1.value);
-  console.log(a1.formula);
-  console.log(a1.formattedValue);
-  console.log(c6.value);
-  console.log(c6.formula);
-  console.log(c6.formattedValue);
+  const rowsCount = (await sheet.getRows()).length;
+  const columnsCount = 10;
 
-  res.status(200).json(a1.value);
+  // array of length rowsCount * columnsCount
+  const data = Array(rowsCount)
+    .fill(0)
+    .map((_, row) =>
+      Array(columnsCount)
+        .fill(0)
+        .map((_, col) => sheet.getCell(row + 1, col).value)
+    );
+
+  res.status(200).json(data);
 }
