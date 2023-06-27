dd9cddf6c6e696e8fb3fe53423ef4f28b17de991
Monika Kinclová <kinclova.monca@gmail.com>
2023-06-27 09:52:05 +0200

    aktualizace, chyba 431

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-3xl font-bold">Penaltový král Moravskoslezského hraje 2023</h1>
+      </main>
+    )
+  }
+  
+  
+  
