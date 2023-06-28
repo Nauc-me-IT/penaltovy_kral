21d1ab0fdac0c1b823bfc5198835b1aa91402eb3
Monika Kinclová <kinclova.monca@gmail.com>
2023-06-28 17:34:44 +0200

    NPM run build

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,22 +5,13 @@ export default async function handler(req, res) {
     "1s6GBtj7ItUPdbnYUGZ7ghzAtbwCO-PKU4RqivCXdotI"
   );
   doc.useApiKey("AIzaSyBP6A-1rMWSR2Oi2maHP0KIk73Nn5_Psbc");
-  await doc.getInfo();
+  await doc.loadInfo(); // Přepnuli jsme z getInfo() na loadInfo()
 
   const sheet = doc.sheetsByIndex[0];
-  await sheet.loadCells("A2:J50");
+  await sheet.loadCells("A15"); // Načte pouze buňku A15
 
-  const rowsCount = (await sheet.getRows()).length;
-  const columnsCount = 10;
+  const cell = sheet.getCellByA1("A15");
+  const value = cell.value;
 
-  // array of length rowsCount * columnsCount
-  const data = Array(rowsCount)
-    .fill(0)
-    .map((_, row) =>
-      Array(columnsCount)
-        .fill(0)
-        .map((_, col) => sheet.getCell(row + 1, col).value)
-    );
-
-  res.status(200).json(data);
+  res.status(200).json({ value });
 }
---
+++ pages/api/data_proces.js
@@ -0,0 +1,17 @@
+export const getStaticProps = async ()=> {
+
+    const res = await fetch ();
+    const data = await res.json()
+}
+
+const Data = () => {
+    return (
+       <div>
+        <h1>Nevím</h1>
+
+       </div> 
+    )
+
+}
+
+export default Data
