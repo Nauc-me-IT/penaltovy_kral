485894f71f2971104b7404fcc4378a7767d587d8
AnthonyNet <zejda.antonin@gmail.com>
2023-07-04 17:23:28 +0200

    refactor(data.js): add new variable´s names and commentary
    commentary from better understanding which data we get

--- pages/api/data.js
+++ pages/api/data.js
@@ -6,42 +6,29 @@ export default async function totalPlayers(req, res) {
 	);
 	doc.useApiKey("AIzaSyBP6A-1rMWSR2Oi2maHP0KIk73Nn5_Psbc");
 	await doc.loadInfo(); // Přepnuli jsme z getInfo() na loadInfo()
-// refactor the code below
-	let List_D2 = doc.sheetsByIndex[2];
-	await List_D2.loadCells("C2:D2");
+	// Select menu on list 2 VYBRAT_KATEGORII
+	//Sends number of chosen list
+	let vyberList = doc.sheetsByIndex[2];
+	await vyberList.loadCells("C2:D2");
 
+	const list = vyberList.getCellByA1("D2");
+	const category = vyberList.getCellByA1("C2");
 
-	const list = List_D2.getCellByA1("D2");
-	const category = List_D2.getCellByA1("C2");
-
-	const sheet = doc.sheetsByIndex[list.value];
-    await sheet.loadCells("A3:I3"); // Načteme buňky A15 až C15
-
-
-	const A3_column = doc.sheetsByIndex[list.value];
-	await A3_column.loadCells("AF3:AG4");
-
-	const AI3_column = doc.sheetsByIndex[list.value];
-	await AI3_column.loadCells("AI3:AL5");
-
-	const pokus = doc.sheetsByIndex[list.value];
-	await pokus.loadCells("AJ2:AJ3");
-/*
-	const vysledky = doc.sheetsByIndex[list.value];
-	await vysledky.loadCells("A4:B13");
-
-	//Results
-	const vysledkyNR = vysledky.getCellByA1("A4");
-	const vysledkyName = vysledky.getCellByA1("B4");*/
+	//Data for Stats.tsx component
+	const stats = doc.sheetsByIndex[list.value];
+	await stats.loadCells("A3:C3");
 
 	// Save data into variables
-	const totalPlayers = sheet.getCellByA1("A3");
-	const restPlayers = sheet.getCellByA1("B3");
-	const round = sheet.getCellByA1("C3");
+	const totalPlayers = stats.getCellByA1("A3");
+	const restPlayers = stats.getCellByA1("B3");
+	const round = stats.getCellByA1("C3");
 
+  //Select players for ARENA
+	const arena = doc.sheetsByIndex[list.value];
+	await arena.loadCells("AF3:AG4");
 
-const shooter_1_name = A3_column.getCellByA1("AG3");
-
+		// Check what data exact cell returns
+	const shooter_1_name = arena.getCellByA1("AG3");
 	const shooter_1_split =
 		shooter_1_name.value === "" ||
 		shooter_1_name.value === null ||
@@ -49,8 +36,7 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			? ["", ""]
 			: shooter_1_name.value.split(". ");
 
-
-	const shooter_2_name = A3_column.getCellByA1("AG4");
+	const shooter_2_name = arena.getCellByA1("AG4");
 	const shooter_2_split =
 		shooter_2_name.value === "" ||
 		shooter_2_name.value === null ||
@@ -58,13 +44,18 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			? ["", ""]
 			: shooter_2_name.value.split(". ");
 
-	const nextPlayer1 = AI3_column.getCellByA1("AJ3");
-	const nextPlayer2 = AI3_column.getCellByA1("AJ4");
-	const nextPlayer3 = AI3_column.getCellByA1("AJ5");
-	const nextPlayer4 = AI3_column.getCellByA1("AK3");
-	const nextPlayer5 = AI3_column.getCellByA1("AK4");
-	const nextPlayer6 = AI3_column.getCellByA1("AK5");
+	const pripraviSe = doc.sheetsByIndex[list.value];
+	await pripraviSe.loadCells("AI3:AL5");
 
+	//Select cells for PRIPRAVI-SE
+	const nextPlayer1 = pripraviSe.getCellByA1("AJ3");
+	const nextPlayer2 = pripraviSe.getCellByA1("AJ4");
+	const nextPlayer3 = pripraviSe.getCellByA1("AJ5");
+	const nextPlayer4 = pripraviSe.getCellByA1("AK3");
+	const nextPlayer5 = pripraviSe.getCellByA1("AK4");
+	const nextPlayer6 = pripraviSe.getCellByA1("AK5");
+
+	// Check incomming data from cells
 	const nextPlayer1_Split =
 		nextPlayer1.value === "" ||
 		nextPlayer1.value === null ||
@@ -79,12 +70,12 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			? ["", ""]
 			: nextPlayer2.value.split(". ");
 
-		const nextPlayer3_Split =
-			nextPlayer3.value === "" ||
-			nextPlayer3.value === null ||
-			nextPlayer3.value === undefined
-				? ["", ""]
-				: nextPlayer3.value.split(". ");
+	const nextPlayer3_Split =
+		nextPlayer3.value === "" ||
+		nextPlayer3.value === null ||
+		nextPlayer3.value === undefined
+			? ["", ""]
+			: nextPlayer3.value.split(". ");
 
 	const nextPlayer4_Split =
 		nextPlayer4.value === "" ||
@@ -93,22 +84,21 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			? ["", ""]
 			: nextPlayer4.value.split(". ");
 
-		const nextPlayer5_Split =
-			nextPlayer5.value === "" ||
-			nextPlayer5.value === null ||
-			nextPlayer5.value === undefined
-				? ["", ""]
-				: nextPlayer5.value.split(". ");
+	const nextPlayer5_Split =
+		nextPlayer5.value === "" ||
+		nextPlayer5.value === null ||
+		nextPlayer5.value === undefined
+			? ["", ""]
+			: nextPlayer5.value.split(". ");
 
-			const nextPlayer6_Split =
-				nextPlayer6.value === "" ||
-				nextPlayer6.value === null ||
-				nextPlayer6.value === undefined
-					? ["", ""]
-					: nextPlayer6.value.split(". ");
+	const nextPlayer6_Split =
+		nextPlayer6.value === "" ||
+		nextPlayer6.value === null ||
+		nextPlayer6.value === undefined
+			? ["", ""]
+			: nextPlayer6.value.split(". ");
 
-
-// export a JSON object with the data we need
+	// export a JSON object with the data we need
 	res.status(200).json({
 		category: category.value,
 		stats: {
@@ -129,7 +119,7 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			nextPlayer4_NR: nextPlayer4_Split[0],
 			nextPlayer5_NR: nextPlayer5_Split[0],
 			nextPlayer6_NR: nextPlayer6_Split[0],
-	},
+		},
 		nextPlayers_Name: {
 			nextPlayer1_Name: nextPlayer1_Split[1],
 			nextPlayer2_Name: nextPlayer2_Split[1],
@@ -138,5 +128,5 @@ const shooter_1_name = A3_column.getCellByA1("AG3");
 			nextPlayer5_Name: nextPlayer5_Split[1],
 			nextPlayer6_Name: nextPlayer6_Split[1],
 		},
-});
+	});
 }
